984c8a24d13fabf4b6d7b9ed03da4d189c71a156
syumai <syumai@gmail.com>
2016-10-01 15:43:48 +0900

    Added callback support

--- example/example.js
+++ example/example.js
@@ -2,11 +2,17 @@
 
 const parse = require('../lib/main.js').parse;
 
+// FFmpegPath is customizable
 const options = {
 	ffmpegPath: null
 }
 
+// Promise usage
 parse(options).then(
-	result => console.log(result)
+	(result) => console.log(result)
 );
 
+// Callback usage
+parse(options, 
+	(result) => console.log(result)
+);
